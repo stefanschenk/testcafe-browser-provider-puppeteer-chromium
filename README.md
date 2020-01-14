44829840e642b97d44dafbdf0a3f1ccb18b8fa8d
Stefan Schenk <stefan@Stefans-MacBook-Pro.local>
2020-01-14 16:23:22 +0100

    Wrote the required methods for the browser provider

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # testcafe-browser-provider-puppeteer-chromium
+
 ![](https://github.com/stefanschenk/testcafe-browser-provider-puppeteer-chromium/workflows/Node%20CI/badge.svg)
 
 This is the **puppeteer-chromium** browser provider plugin for [TestCafe](http://devexpress.github.io/testcafe).
+
+# Getting started
+
+## Installation
+
+To use the puppeteer-chromium provider in your project, run:
+
+```
+npm install --save-dev testcafe-browser-provider-puppeteer-chromium
+```
+
+This will install the latest version of this provider, combined with the latest version of puppeteer.
+
+If you want to install a specific version of this provider to run TestCafe tests against a specific chromium version, you can install the provider using a npm tag.
+
+For example, in order to test against the chromium revision that is compatible with Chrome 77 with this provider, use `chrome-77` npm tag:
+
+```
+npm install --save-dev testcafe-browser-provider-puppeteer-chromium@chrome-77
+```
+
+_info: not all chrome tags are supported at this moment_
 
 ## Puppeteer
 
@@ -17,23 +40,20 @@ npm install testcafe-browser-provider-puppeteer-chromium
 
 ## Usage
 
-
 When you run tests from the command line, use the provider name when specifying browsers:
 
 ```
 testcafe puppeteer-chromium 'path/to/test/file.js'
 ```
 
-
 When you use API, pass the provider name to the `browsers()` method:
 
 ```js
 testCafe
-    .createRunner()
-    .src('path/to/test/file.js')
-    .browsers('puppeteer-chromium')
-    .run();
+  .createRunner()
+  .src('path/to/test/file.js')
+  .browsers('puppeteer-chromium')
+  .run();
 ```
 
 ## Author
- 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,87 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { resolve } from 'path';
+import puppeteer from 'puppeteer';
+import browserTools from 'testcafe-browser-tools';
+
+function getScreenSize() {
+  return { width: screen.availWidth, height: screen.availHeight };
+}
+
 export default {
+  browser: null,
+  pages: {},
+  screenSizes: {},
+
   // Multiple browsers support
   isMultiBrowser: false,
 
-  // Required - must be implemented
-  // Browser control
-  async openBrowser(/* id, pageUrl */) {
-    throw new Error('Not implemented!');
+  /**
+   * Required. Method to open the browser.
+   * This provider, by default, opens the browser visible (not headless) and maximized
+   *
+   * If you want this provider to open the browser headless with a default window-size of
+   * 1920x1080, pass the argument: `ci=true`
+   *
+   * @param {string} id
+   * @param {string} pageUrl
+   * @param {object} config
+   */
+  async openBrowser(id, pageUrl, config) {
+    //  Default launch options, values can be overwritten or add by submitting a .chromium.js file
+    //  disable-infobars does not work on chrome 77 and later, therefore the option '--enable-automation' is ignored
+    //  and '--no-default-browser-check' is added to the args.
+    const defaultLaunchOptions = {
+      args: ['--disable-infobars', '--no-default-browser-check'],
+      defaultViewport: null,
+      headless: false,
+      ignoreDefaultArgs: ['--enable-automation'],
+      timeout: 30000,
+    };
+    let launchOptions = {};
+
+    //  Launch the browser if not yet launched
+    if (!this.browser) {
+      if (!config) {
+        launchOptions = defaultLaunchOptions;
+      } else {
+        try {
+          const configPath = resolve(config || '.');
+          const launchOptionsFromConfig = require(configPath).config.chromium;
+
+          launchOptions = { ...defaultLaunchOptions, ...launchOptionsFromConfig };
+        } catch (err) {
+          throw new Error('Error reading the launch options from the config file!' + err);
+        }
+      }
+
+      this.browser = await puppeteer.launch(launchOptions);
+    }
+
+    //  Open the TestCafe proxy index page
+    const pages = await this.browser.pages();
+    const page = pages[0];
+
+    await page.goto(pageUrl);
+
+    //  Wait until the browser is ready before continuing
+    this.waitForConnectionReady(id);
+
+    //  Maximize the browser window
+    this.maximizeWindow(id);
+
+    //  Save the opened page for further actions
+    this.pages[id] = page;
   },
 
-  async closeBrowser(/* id */) {
-    throw new Error('Not implemented!');
+  async closeBrowser(id) {
+    await this.pages[id].close();
+    delete this.pages[id];
+
+    await this.browser.close();
   },
 
-  // Optional - implement methods you need, remove other methods
-  // Initialization
+  // Optional initialization and cleanup methods
   async init() {
     return;
   },
@@ -23,15 +91,33 @@ export default {
   },
 
   // Extra methods
-  async resizeWindow(/* id, width, height, currentWidth, currentHeight */) {
-    this.reportWarning(
-      'The window resize functionality is not supported by the "puppeteer-chromium" browser provider.',
-    );
+  async maximizeWindow(id) {
+    await browserTools.maximize(id);
+
+    this.screenSizes[id] = await this.runInitScript(id, getScreenSize.toString());
+    this.screenSizes[id].height = Math.ceil(this.screenSizes[id].height * 0.927);
+
+    await this.pages[id].setViewport(this.screenSizes[id]);
   },
 
-  async takeScreenshot(/* id, screenshotPath, pageWidth, pageHeight */) {
-    this.reportWarning(
-      'The screenshot functionality is not supported by the "puppeteer-chromium" browser provider.',
-    );
+  async canResizeWindowToDimensions(id, width, height) {
+    var { width: screenWidth, height: screenHeight } = this.screenSizes[id];
+
+    return width <= screenWidth && height <= screenHeight;
+  },
+
+  async resizeWindow(id, width, height) {
+    await this.pages[id].setViewport({ width, height });
+  },
+
+  async takeScreenshot(id, screenshotPath, pageWidth, pageHeight, fullPage) {
+    const dir = path.dirname(screenshotPath);
+
+    fs.mkdirSync(dir, { recursive: true });
+
+    await this.pages[id].screenshot({
+      path: screenshotPath,
+      fullPage: fullPage,
+    });
   },
 };
